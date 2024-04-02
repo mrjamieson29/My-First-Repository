fb54ac365dcde58ced36c8b60403f644eae175cf
mrjamieson29 <157923825+mrjamieson29@users.noreply.github.com>
2024-04-02 10:34:07 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+/**
+ * ICS3UC - Mr. J 🐠
+ * 
+ * Make sure you read the README and YOU_TASK files. * 
+ * 
+ * We want the code below to output "I love to code!"
+ */
+
+
+conlsole.lag("I hate to code!");
